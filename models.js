1ec009f622e0e67dc15d4079d3b3b6feb663f72c
chansiky <chansiky@gmail.com>
2018-04-30 16:22:03 -0500

    sequelize: database connected

--- models.js
+++ models.js
@@ -1,1 +1,29 @@
-hello world
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/plantr');
+const Gardener = db.define('Gardener',{
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
+
+const Plot = db.define('Plot',{
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = db.define('Vegetable' ,{
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, {through: "land"});
+Plot.belongsToMany(Vegetable, {through: "land"});
+
+Gardener.belongsTo(Vegetable, {as: "favorite_vegetable"});
+
+
+//potential conflict "may" belong to many
+module.exports = db;
