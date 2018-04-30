4e84aefee8797efdebb4f8ddead18d80e6cc4566
Homum Ahsan <hahsan95@gmail.com>
2018-04-30 17:24:48 -0500

    doneish

--- plantr/seed.js
+++ plantr/seed.js
@@ -6,7 +6,11 @@ db.sync({force:true})
   .then(() => {
     return Vegetable.create({name: 'Kale', color: 'Green'});
   })
-  .then((veg) => console.log(veg))
+  .then(() => {
+    return veg => Vegetable.create({name: 'Squash', color: 'Yellow'});
+  })
+  .then(veg => Gardener.create({ name: 'Chan', age: '31', favoriteVegetableId: veg.id}))
+  .then(veg => Gardener.create({ name: 'George', age: '21', favoriteVegetableId: veg.id}))
   .then(() => {
     console.log('database synced');
     db.close();
